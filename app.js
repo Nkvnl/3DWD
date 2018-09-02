472f726dd54c74ac780af4d74c4e6e4fa56972ba
Niek van Losenoord <niek_losenoord@hotmail.com>
2018-09-02 13:26:53 +0000

    addblog

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express    = require("express"); // call express
+var express = require("express"); // call express
 var bodyParser = require("body-parser");
 var nodemailer = require("nodemailer");
 // var compressible = require('compressible')
 // const exphbs     = require("handlebars");
-var app        = express();
+var app = express();
 
 
 
@@ -18,25 +18,76 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("theme-blue");
 });
 
-app.get("/red", function(req, res){
+app.get("/red", function(req, res) {
     res.render("theme-red");
 });
 
-app.get("/green", function(req, res){
+app.get("/green", function(req, res) {
     res.render("theme-green");
 });
 
-app.get("/yellow", function(req, res){
+app.get("/yellow", function(req, res) {
     res.render("theme-yellow");
 });
 
-app.get("/test", function(req, res){
+app.get("/test", function(req, res) {
     res.render("bedankt");
 });
+
+app.get("/blog", function(req, res) {
+    res.render("blog")
+})
+
+app.get("/website", function(req, res) {
+    res.render("website")
+})
+
+
+app.post("/signup", (req, res) => {
+    var name = (req.body.name);
+    var output = `
+    <h3> Nieuwe aanmelding nieuwsbrief<h3>
+    <h5>${req.body.email}<h5>
+    `
+
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: 'mailserver163@gmail.com',
+            pass: 'ma1ls3rv3r'
+        }
+    });
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"3DWD" <mailserver163@gmail.com>', // sender address
+        to: 'niek_losenoord@hotmail.com', // list of receivers
+        subject: 'Aanmelding nieuwsbrief', // Subject line
+        text: '', // plain text body
+        html: output // html body
+    };
+
+    var msg = "Bedankt voor je bericht! Binnen 24 uur zijn we bij je terug."
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+        res.render("bedankt")
+
+    });
+});
+
 
 app.post("/send", (req, res) => {
     var name = (req.body.name);
@@ -50,16 +101,16 @@ app.post("/send", (req, res) => {
     </ul>
     <p>${req.body.bericht}<p>
     `
-    
+
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: 'mailserver163@gmail.com',
-        pass: 'ma1ls3rv3r'
-    }
-});
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: 'mailserver163@gmail.com',
+            pass: 'ma1ls3rv3r'
+        }
+    });
     // setup email data with unicode symbols
     let mailOptions = {
         from: '"3DWD" <mailserver163@gmail.com>', // sender address
@@ -79,9 +130,9 @@ app.post("/send", (req, res) => {
 
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        
+
         res.render("bedankt")
-        
+
     });
 });
 
@@ -118,6 +169,6 @@ app.post("/send", (req, res) => {
 //   smtpTransport.close(); 
 //     }); });
 
-app.listen(process.env.PORT, process.env.IP, function(){ // tell node to listen & define a port to view app
+app.listen(process.env.PORT, process.env.IP, function() { // tell node to listen & define a port to view app
     console.log("3D Web Dev server starting...");
 });
